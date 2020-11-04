a7767aaecb6ad973e5cd17b3b92451fbd99530c8
Zurab Dzindzibadze <zuradzindzibadze06@gmail.com>
2020-11-04 21:26:15 +0400

    added removing elemnt from anywhere O(n)

--- include/SLinkedList.hpp
+++ include/SLinkedList.hpp
@@ -1,5 +1,5 @@
-#include <fmt/format.h> // TODO: BLOAT WITH IOSTREAM ?? 
-#include <new>  // new is only for educational purpuses
+#include <fmt/format.h> // TODO: BLOAT WITH IOSTREAM ??
+#include <new>          // new is only for educational purpuses
 
 template <typename T> class SingleLinkedList {
 private:
@@ -19,6 +19,7 @@ public:
   [[nodiscard]] bool empty() const noexcept { return head == nullptr; }
   [[nodiscard]] T const &front() const noexcept { return head->elem; }
 
+  void removeElement(T const &e) noexcept;
   void addFront(T const &e) noexcept;
   void removeFront() noexcept;
   void traverse() const noexcept;
@@ -26,6 +27,29 @@ public:
 private:
   Node *head;
 };
+
+template <typename T>
+void SingleLinkedList<T>::removeElement(const T &e) noexcept {
+  Node *del_ptr = head;
+
+  if (head->elem == e) {
+    head = head->next;
+    delete del_ptr;
+    return;
+  }
+
+  Node *predacessor;
+
+  while (del_ptr != nullptr) {
+    predacessor = del_ptr;
+    del_ptr = del_ptr->next;
+    if (del_ptr->elem == e) {
+      predacessor->next = del_ptr->next;
+      delete del_ptr;
+      break;
+    }
+  }
+}
 
 template <typename T> void SingleLinkedList<T>::addFront(T const &e) noexcept {
   Node *v = new (std::nothrow) Node;
@@ -42,7 +66,7 @@ template <typename T> void SingleLinkedList<T>::removeFront() noexcept {
 }
 
 template <typename T> void SingleLinkedList<T>::traverse() const noexcept {
-  Node* ptr = head;
+  Node *ptr = head;
   while (ptr != nullptr) {
     fmt::print("{}\n", ptr->elem);
     ptr = ptr->next;
