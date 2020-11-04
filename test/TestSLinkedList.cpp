e141f845e759f46067634f08ef9966b29fbd0cf3
Zurab Dzindzibadze <zuradzindzibadze06@gmail.com>
2020-11-04 21:26:51 +0400

    testing removing elemnt from anywhere

--- test/TestSLinkedList.cpp
+++ test/TestSLinkedList.cpp
@@ -8,28 +8,60 @@ TEST_CASE("SLinkedList correctly maintains elments") {
   SECTION("Initial list is empty") { REQUIRE(list.empty()); }
 
   SECTION("Element adding and removing works") {
-    constexpr int a1{69};
-
-    list.addFront(a1);
-    REQUIRE(list.front() == a1);
+    list.addFront(69);
+    REQUIRE(list.front() == 69);
 
     list.removeFront();
     REQUIRE(list.empty());
   }
 
   SECTION("Sequential element adding gives predictable structure") {
-    constexpr int a1{69};
-    constexpr int a2{42};
+    list.addFront(69);
+    list.addFront(42);
 
-    list.addFront(a1);
-    list.addFront(a2);
-
-    REQUIRE(list.front() == a2);
+    REQUIRE(list.front() == 42);
     list.removeFront();
 
-    REQUIRE(list.front() == a1);
+    REQUIRE(list.front() == 69);
     list.removeFront();
 
     REQUIRE(list.empty());
+  }
+}
+
+TEST_CASE("SLinkedList removing elements from arbitrary place") {
+  SingleLinkedList<int> list;
+
+  SECTION("Removing head element correctly") {
+    list.addFront(42);
+    list.removeElement(42);
+    REQUIRE(list.empty());
+
+    list.addFront(42);
+    list.addFront(13);
+    list.removeElement(13);
+    REQUIRE(list.front() == 42);
+    list.removeFront();
+  }
+
+  SECTION("Removing last element correctly") {
+    list.addFront(42);
+    list.addFront(13);
+
+    list.removeElement(42);
+    REQUIRE(list.front() == 13);
+    list.removeFront();
+  }
+
+  SECTION("Removing arbitrary element correctly") {
+    list.addFront(42);
+    list.addFront(13);
+    list.addFront(15);
+
+    list.removeElement(13);
+    REQUIRE(list.front() == 15);
+    list.removeFront();
+    REQUIRE(list.front() == 42);
+    list.removeFront();
   }
 }
